a852781690958c34192be2a52340a1988d7ee3b4
Maureen E. Mirville <mirvil7130@aol.com>
2017-02-15 18:09:17 -0500

    added csv file

--- public/bundle.js
+++ public/bundle.js
@@ -9531,8 +9531,6 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-//import allQuestions from '../data/allQuestions.csv';
-
 var Questions = function (_React$Component) {
   _inherits(Questions, _React$Component);
 
